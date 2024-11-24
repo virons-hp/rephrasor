e702c41ec96bb11a67c84b35371008027eddaf23
virons-hp <taknikwala@gmail.com>
2024-11-25 00:36:08 +0530

    tool bar added

--- frontend/src/components/MainSection.jsx
+++ frontend/src/components/MainSection.jsx
@@ -6,30 +6,32 @@ const MainSection = () => {
 
   return (
     <>
-      <section className="mainSection">
-        <div className="toolContainer">
+      <section className="mainSection w-full flex justify-center align-middle items-center">
+        <div className="toolContainer bg-gray-500 w-[80%]">
           <div className="toolContainerTopSection">
-            <div>
-              <div className=""></div>
-              <div className=""></div>
-              <div className=""></div>
-              <div className=""></div>
-              <div className=""></div>
+            <div className="toolContainerTopSectionButtons py-4 bg-pink-400 flex flex-row justify-evenly">
+              <div className=""><p>Type:-</p></div>
+              <div className=""><button type="button">Informal</button></div>
+              <div className=""><button type="button">Formal</button></div>
+              <div className=""><button type="button">Creative</button></div>
+              <div className=""><button type="button"> Concise</button></div>
             </div>
           </div>
 
-          <div className="toolContainerBottomSection">
+          <div className="toolContainerBottomSection p-2">
             <div className="toolContainerBottomMain">
-              <div className="promptContainer flex flex-row gap-1 justify-center ">
+              <div className="promptContainer flex flex-row gap-4 justify-center ">
                 <div className="inputContainer w-[40%] bg-blue-400">
+
                   <div className="inputBox">
                     <textarea
                       className="w-full p-4 border rounded"
                       placeholder="Enter text to rephrase"
                       rows="16"
                     ></textarea>
+
                   </div>
-                  <div className="inputContainerButtons w-full flex flex-row gap-2 py-2 ">
+                  <div className="inputContainerButtons w-full flex flex-row gap-2 py-4 justify-evenly ">
                     <div className="wordCountDiv"><span className="font-semibold ">words:</span> 100/200</div>
                     <div className="restButtonDiv">
                       {" "}
